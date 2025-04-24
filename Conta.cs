eca36842dbfced41cccf2fc5e69b184f9faf231d
Welliton Giori <gioriwelliton47@gmail.com>
2025-04-23 20:22:39 -0400

    Inicio do projeto

---
+++ Conta.cs
@@ -0,0 +1,58 @@
+﻿public class Conta
+{
+    public int _id;
+    public string _agencia;
+    public string _numeroConta;
+    public string _nomePropietario;
+    private double _saldo;
+
+    public void Depositar(double valor)
+    {
+        try
+        {
+            if (valor <= 0)
+            {
+                throw new Exception("Valores negativos/zero não são permitidos!");
+            }
+
+            _saldo += valor;
+            Console.Clear();
+            Console.WriteLine(valor.ToString("C2") + " Depositado com sucesso!");
+            Console.WriteLine("Saldo Atual: " + GetSaldo().ToString("C2"));
+
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void Sacar(double valor)
+    {
+        try
+        {
+            if (valor <= 0)
+            {
+                throw new Exception("Valores negativos/zero não são permitidos");
+            }
+            if (_saldo < valor)
+            {
+                throw new Exception("O valor de saque é maior do que o saldo!\nSaldo Disponível: " + GetSaldo().ToString("C2"));
+            }
+
+            _saldo -= valor;
+            Console.Clear();
+            Console.WriteLine(valor.ToString("C2") + " Sacado com sucesso!");
+            Console.WriteLine("Saldo Atual: " + GetSaldo().ToString("C2"));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public double GetSaldo()
+    {
+        return _saldo;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("SISTEMA DE CONTA BANCÁRIA");
+Console.WriteLine("");
+
+List<Conta> lContas = new List<Conta>();    
+
+while (true)
+{    
+    Console.WriteLine("Digite 1 para cadastrar, 2 para listar, 3 para depositar, 4 para sacar, e 5 para sair!");
+    int opc = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+
+    if (opc == 1)
+    {
+        Console.WriteLine("CADASTRAR NOVA CONTA");
+        Console.WriteLine("");
+
+        Conta conta = new Conta();
+
+        int idAutoIncrement = 0;
+        Conta ultimaConta = lContas.FindLast(c => c._id == c._id);
+        if (ultimaConta != null)
+        {
+            idAutoIncrement = ultimaConta._id;
+        }
+
+        conta._id = idAutoIncrement+1;
+
+        Console.Write("Digite o número da agência: ");
+        conta._agencia = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.Write("Digite o número da conta: ");
+        conta._numeroConta = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.Write("Digite seu nome: ");
+        conta._nomePropietario = Console.ReadLine();
+        lContas.Add(conta);
+    }
+    else if (opc == 2)
+    {
+        foreach (Conta c in lContas)
+        {
+            Console.WriteLine($"{c._id} | {c._numeroConta} | {c._nomePropietario} | {c.GetSaldo().ToString("C2")} \n");
+        }
+
+        Console.ReadKey();
+    }
+    else if (opc == 3)
+    {
+        Console.WriteLine("DEPOSITAR");
+        Console.WriteLine("");
+        Console.Write("Digite a agência: ");
+        string agencia = Console.ReadLine();
+        Console.Write("Digite o número da conta: ");
+        string numero = Console.ReadLine();
+
+        Conta conta = lContas.Where(c => c._agencia == agencia).Where(c => c._numeroConta == numero).FirstOrDefault();
+
+        if (conta != null)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Conta: {conta._nomePropietario}");
+            Console.Write("Digite o valor de depósito: ");
+            conta.Depositar(Convert.ToDouble(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Conta não encontrada!");
+            Console.ReadKey();
+        }
+    }
+    else if (opc == 4)
+    {
+        Console.WriteLine("SACAR");
+        Console.WriteLine("");
+        Console.Write("Digite a agência: ");
+        string agencia = Console.ReadLine();
+        Console.Write("Digite o número da conta: ");
+        string numero = Console.ReadLine();
+
+        Conta conta = lContas.Where(c => c._agencia == agencia).Where(c => c._numeroConta == numero).FirstOrDefault();
+
+        if (conta != null)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Conta: {conta._nomePropietario}");
+            Console.Write("Digite o valor de saque: ");
+            conta.Sacar(Convert.ToDouble(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Conta não encontrada!");
+            Console.ReadKey();
+        }
+    }
+    else
+    {
+        break;
+    }
+
+    Console.Clear();
+}
+
+/*
+while (true)
+{
+    
+}
+*/
